c86e5bb62c9a18fec329e29894b1002b9ad6a86f
Randy Tozuka <kazm1208@me.com>
2020-11-01 14:03:06 +0900

    1st Nov 2020 -2

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,9 @@
 class BookingsController < ApplicationController
   def index
-    @bookings = current_user.bookings.all
+    if current_user
+      @bookings = current_user.bookings.all
+    end
+
   end
 
   def show
@@ -8,7 +11,16 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
-    
+    date = "2020-11-05"
+    @booking_count1 = Booking.where("date LIKE?", "%#{date}%").where(slot:"11:30~11:45").count
+    @booking_count2 = Booking.where("date LIKE?", "%#{date}%").where(slot:"11:45~12:00").count
+    @booking_count3 = Booking.where("date LIKE?", "%#{date}%").where(slot:"12:00~12:15").count
+    @booking_count4 = Booking.where("date LIKE?", "%#{date}%").where(slot:"12:15~12:30").count
+    @booking_count5 = Booking.where("date LIKE?", "%#{date}%").where(slot:"12:30~12:45").count
+    @booking_count6 = Booking.where("date LIKE?", "%#{date}%").where(slot:"12:45~13:00").count
+    @booking_count7 = Booking.where("date LIKE?", "%#{date}%").where(slot:"13:00~13:15").count
+    @booking_count8 = Booking.where("date LIKE?", "%#{date}%").where(slot:"13:15~13:30").count
+
   end
 
   def create
