468492e210de714c32eeef212dfc71131f21cf8f
Randy Tozuka <kazm1208@me.com>
2020-11-04 06:23:57 +0900

    4th Nov 2020-0

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module BookingApp
 
     # タイムゾーンを日本時間に設定
     config.time_zone = 'Asia/Tokyo'
-
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
