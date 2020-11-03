d36164297546fc4f13956cb34d65e950af698931
Randy Tozuka <kazm1208@me.com>
2020-11-03 18:53:16 +0900

    3rd Nov 2020-0

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,11 +3,20 @@ class Booking < ApplicationRecord
   validates :user_id, presence: true
   validates :date,{presence:true}
   validates :slot,{presence:true}
-  validate :check_num_of_a_slot
+  # validate :check_num_of_a_slot
+  #
+  # def check_num_of_a_slot
+  #   if Booking.slot.count > 4
+  #     errors.add(:slot, "Choose other slots")
+  #   end
+  # end
 
   def check_num_of_a_slot
-    if Booking.slot.count > 150
+    selected_date = "2020-11-01 15:00"
+    selected_slot = "11:30~11:45"
+    if Booking.where(date: selected_date).where(slot: selected_slot).count > 3
       errors.add(:slot, "Choose other slots")
     end
   end
+
 end
