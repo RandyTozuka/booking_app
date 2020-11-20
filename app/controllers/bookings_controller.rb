65a167360519c70e73b9cd0be75df2e71940d1e9
Randy Tozuka <kazm1208@me.com>
2020-11-21 07:04:55 +0900

    21st Nov 2020

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,31 +11,46 @@ class BookingsController < ApplicationController
 
   def new
     @booking = current_user.bookings.new
-    date = "2020-11-02"
-    @booking_count1 = Booking.where("date LIKE?", "%#{date}%").where(slot:"11:30~11:45").count
-    @booking_count2 = Booking.where("date LIKE?", "%#{date}%").where(slot:"11:45~12:00").count
-    @booking_count3 = Booking.where("date LIKE?", "%#{date}%").where(slot:"12:00~12:15").count
-    @booking_count4 = Booking.where("date LIKE?", "%#{date}%").where(slot:"12:15~12:30").count
-    @booking_count5 = Booking.where("date LIKE?", "%#{date}%").where(slot:"12:30~12:45").count
-    @booking_count6 = Booking.where("date LIKE?", "%#{date}%").where(slot:"12:45~13:00").count
-    @booking_count7 = Booking.where("date LIKE?", "%#{date}%").where(slot:"13:00~13:15").count
-    @booking_count8 = Booking.where("date LIKE?", "%#{date}%").where(slot:"13:15~13:30").count
+    @booking_count1 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"11:30~11:45").count
+    @booking_count2 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"11:45~12:00").count
+    @booking_count3 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"12:00~12:15").count
+    @booking_count4 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"12:15~12:30").count
+    @booking_count5 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"12:30~12:45").count
+    @booking_count6 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"12:45~13:00").count
+    @booking_count7 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"13:00~13:15").count
+    @booking_count8 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"13:15~13:30").count
+    #binding.pry
   end
 
-  def create 
+  def create
     @user = current_user
-    @booking = @user.bookings.find_by(params[user_id: booking_params])
-    binding.pry
-    if Booking.where(user_id: @user.id).where(date: @booking.date).any?
-      flash[:danger]= "Double booking in the the day! Please check."
+    @booking_date = params[:booking][:date]
+    @booking_slot = params[:booking][:slot]
+    #binding.pry
+    # 予約ができる日は本日以降の未来とする
+    if @booking_date <  Date.today.to_s
+      flash[:danger]= "Head for the future!"
+      redirect_to new_booking_path
+    # @booking_dateはstring型なのでdate型に変換し、wdayで曜日を出し、日曜（0）か土曜（6）は予約をさせない
+    elsif Date.strptime(@booking_date).wday == 0 || Date.strptime(@booking_date).wday == 6
+      flash[:danger]= "Dont' be a slave of your job!"
       redirect_to new_booking_path
     else
-      if current_user.bookings.create(booking_params)
-        flash[:success]= "Successfully booked"
-        redirect_to '/'
+      if Booking.where(user_id: @user.id).where(date: @booking_date).any?
+        flash[:danger]= "Double booking in the the day! Please check."
+        redirect_to new_booking_path
+
+      elsif Booking.where(date:@booking_date).where(slot:@booking_slot).count >= 1
+            flash[:danger]= "That slot is fully occupied! Please try other slot."
+            redirect_to new_booking_path
       else
-        flash[:danger]= "Your booking failed"
-        redirect_to '/bookings/new'
+        if current_user.bookings.create(booking_params)
+          flash[:success]= "Successfully booked"
+          redirect_to '/'
+        else
+          flash[:danger]= "Your booking failed"
+          redirect_to '/bookings/new'
+        end
       end
     end
   end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,23 +2,20 @@ class Booking < ApplicationRecord
 
   belongs_to :user
   validates :user_id, presence: true
-  validates :date,{presence:true}
-  validates :slot,{presence:true}
+  validates :date,    presence: true
+  validates :slot,    presence: true
+  # validate :slot_limit
+  default_scope -> {order(date: :asc)}
 
-  # validate :check_num_of_a_slot
-  #
-  # def check_num_of_a_slot
-  #   if Booking.slot.count > 4
-  #     errors.add(:slot, "Choose other slots")
+  # def slot_limit
+  #   binding.pry
+  #   @booking_date = params[:booking][:date]
+  #   @booking_slot = params[:booking][:slot]
+  #   @bookings = Booking.where(date:@booking_date).where(slot:@booking_slot)
+  #   if @bookings.count > 1
+  #     errors.add(:base, 'That slot is fully occupied! Please try other slot.')
   #   end
+  #
   # end
-
-  def check_num_of_a_slot
-    selected_date = "2020-11-01 15:00"
-    selected_slot = "11:30~11:45"
-    if Booking.where(date: selected_date).where(slot: selected_slot).count > 3
-      errors.add(:slot, "Choose other slots")
-    end
-  end
 
 end
