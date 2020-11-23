91ec53860cc42815002659f212a8c5a5fe1c559c
Randy Tozuka <kazm1208@me.com>
2020-11-23 16:08:59 +0900

    23rd Nov 2020 R2

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,35 +13,26 @@ class BookingsController < ApplicationController
 
   def new
     @booking = current_user.bookings.new
-    # @booking_count1 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"11:30~11:45").count
-    # @booking_count2 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"11:45~12:00").count
-    # @booking_count3 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"12:00~12:15").count
-    # @booking_count4 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"12:15~12:30").count
-    # @booking_count5 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"12:30~12:45").count
-    # @booking_count6 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"12:45~13:00").count
-    # @booking_count7 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"13:00~13:15").count
-    # @booking_count8 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"13:15~13:30").count
-    #binding.pry
   end
 
   def create
     @user = current_user
     @booking_date = params[:booking][:date]
     @booking_slot = params[:booking][:slot]
-    #binding.pry
     # 予約ができる日は本日以降の未来とする
     if @booking_date <  Date.today.to_s
       flash[:danger]= "Head for the future!"
       redirect_to new_booking_path
-    # @booking_dateはstring型なのでdate型に変換し、wdayで曜日を出し、日曜（0）か土曜（6）は予約をさせない
+    # 日曜（0）か土曜（6）は予約をさせない。→@booking_dateはstring型なのでdate型に変換し、wdayで曜日を出す
     elsif Date.strptime(@booking_date).wday == 0 || Date.strptime(@booking_date).wday == 6
       flash[:danger]= "Dont' be a slave of your job!"
       redirect_to new_booking_path
     else
+    #同じ人が同じ日に予約を入れる事を防ぐ
       if Booking.where(user_id: @user.id).where(date: @booking_date).any?
         flash[:danger]= "Double booking in the the day! Please check."
         redirect_to new_booking_path
-
+    #同じ日の同じスロットに入る人数の調整…現在はテストとして1人以上予約が入ることを阻止
       elsif Booking.where(date:@booking_date).where(slot:@booking_slot).count >= 1
             flash[:danger]= "That slot is fully occupied! Please try other slot."
             redirect_to new_booking_path
@@ -88,3 +79,13 @@ class BookingsController < ApplicationController
     end
 
 end# of class
+
+#メモ
+# @booking_count1 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"11:30~11:45").count
+# @booking_count2 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"11:45~12:00").count
+# @booking_count3 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"12:00~12:15").count
+# @booking_count4 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"12:15~12:30").count
+# @booking_count5 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"12:30~12:45").count
+# @booking_count6 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"12:45~13:00").count
+# @booking_count7 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"13:00~13:15").count
+# @booking_count8 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"13:15~13:30").count
